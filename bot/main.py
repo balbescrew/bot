df34e07ea8f7e725df3e00c9102c99a6640415ac
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2025-06-08 11:41:37 +0300

    kafka fix

--- bot/main.py
+++ bot/main.py
@@ -1,63 +1,31 @@
-import json
 import asyncio
 import logging
-import threading
 from logging import getLogger
 
-from quixstreams import Application
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers.message_handler import router as message_router
+from quix_client import consumer
 
 logging.basicConfig(level=logging.INFO)
 logger = getLogger(__name__)
 logger.setLevel("DEBUG")
 
-kafka = Application(
-    broker_address="10.10.127.2:9092",
-)
 
 BOT: Bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 dp.include_router(message_router)
 
 
-async def kafka_consumer_loop(topic: str = "spam_messages"):
-    consumer = kafka.get_consumer(group_id="telegram-consumer")
-    consumer.subscribe([topic])
-    logger.info("Kafka consumer started and subscribed to topic")
-
-    try:
-        while True:
-            result = consumer.poll(timeout=1.0)
-            if result is None:
-                await asyncio.sleep(0.1)
-                continue
-
-            message = json.loads(result.value())
-            logger.info(f"Received message from Kafka: {message}")
-            try:
-                await BOT.delete_message(
-                    message["chat_id"], message["message_id"]
-                )
-                logger.info('сообщение удалилось')
-            except Exception as e:
-                logger.error(f"Failed to delete message: {e}")
-    except Exception as e:
-        logger.error(f"Kafka consumer error: {e}")
-    finally:
-        consumer.close()
-
-
-def start_kafka_consumer():
-    asyncio.run(kafka_consumer_loop())
+async def telegram_loop():
+    await dp.start_polling(BOT)
 
 
 async def main():
-    threading.Thread(target=start_kafka_consumer, daemon=True).start()
-
-    await dp.start_polling(BOT)
-
+    asyncio.gather(
+        telegram_loop(),
+        consumer(bot=BOT)
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
--- bot/quix_client.py
+++ bot/quix_client.py
@@ -1,6 +1,8 @@
+import asyncio
 import json
 from logging import getLogger
 from quixstreams import Application
+from aiogram import Bot
 
 logger = getLogger(__name__)
 logger.setLevel("DEBUG")
@@ -19,3 +21,35 @@ async def send_message_to_kafka(
             value=json.dumps(message_dict),
         )
     logger.debug(f"Produced message to Kafka: {message_dict}")
+
+
+async def consumer(
+        topic: str = "spam_messages", app: Application = kafka, bot: Bot = None
+):
+    app = Application(
+        broker_address="10.10.127.2:9092",
+        loglevel="INFO",
+        consumer_group='delete_bot',
+    )
+
+    with app.get_consumer() as consumer:
+        consumer.subscribe([topic])
+
+        while True:
+            msg = consumer.poll(1)
+            if msg is None:
+                continue
+            elif msg.error() is not None:
+                raise Exception(msg.error())
+
+            message = json.loads(msg.value())
+            logger.info(f"Received message from Kafka: {message}")
+            try:
+                logger.info('сообщение удаляется')
+                asyncio.create_task(bot.delete_message(
+                    message['chat_id'],
+                    message['message_id'],
+                ))
+                logger.info('сообщение удалилось')
+            except Exception as e:
+                logger.error(f"Failed to delete message: {e}")
